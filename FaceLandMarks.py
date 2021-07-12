6bc9fd18de0fec398d87b499414af9086c5ba8a5
yousseff <yousefmasry4>
2021-07-12 18:46:24 +0200

    init

--- FaceLandMarks.py
+++ FaceLandMarks.py
@@ -3,10 +3,14 @@ import numpy as np
 import math
 import mediapipe as mp
 
+
 class FaceLandMarks:
-    def __init__(self):
-        print("init")
-        pass
+    def __init__(self, img):
+        self.img = img
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.mp_face_detection = mp.solutions.face_detection
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=2)
 
 
 # dev
