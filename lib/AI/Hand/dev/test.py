f8fe05726a3230a4cd9f7086b2ded04182b89d92
yousseffel-masry <youssefmasry04@gmail.com>
2021-08-26 20:16:56 +0200

    dev wrist v3 done

--- lib/AI/Hand/dev/test.py
+++ lib/AI/Hand/dev/test.py
@@ -5,6 +5,8 @@ from numpy.linalg import lstsq
 import math
 import numpy as np
 from numpy.linalg import norm
+from sympy import symbols, Eq, solve
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
@@ -39,36 +41,41 @@ with mp_hands.Hands(
 
 def midpoint(p1, p2):
     return (p1.x + p2.x) / 2, (p1.y + p2.y) / 2
+
+
 def limitt(n):
-    if n[0]<=1 and n[0]>=0:
+    if n[0] <= 1 and n[0] >= 0:
         return n[0]
 
-def Dpoint(c,a,b,r):
-    pi=3.1415926535897932384626433832795
+
+def Dpoint(c, a, b, r):
+    pi = 3.1415926535897932384626433832795
     degree = 2 * pi / 360
     radian = 1 / degree
-    radius=  r
-    a0=np.multiply(a,degree)
-    b0=np.multiply(b,degree)
-    c0=np.multiply(c,degree)
-    aa=[1.0,math.cos(c0[0])]
-    ba=[1.0,math.cos(c0[0])]
-    ca= [1.0,math.cos(c0[0])]
-    A=np.multiply(np.multiply(a0,aa),radius)
-    B=np.multiply(np.multiply(b0,ba),radius)
-    C=np.multiply(np.multiply(c0,ca),radius)
-    V=A-C
-    U=B-A
+    radius = r
+    a0 = np.multiply(a, degree)
+    b0 = np.multiply(b, degree)
+    c0 = np.multiply(c, degree)
+    aa = [1.0, math.cos(c0[0])]
+    ba = [1.0, math.cos(c0[0])]
+    ca = [1.0, math.cos(c0[0])]
+    A = np.multiply(np.multiply(a0, aa), radius)
+    B = np.multiply(np.multiply(b0, ba), radius)
+    C = np.multiply(np.multiply(c0, ca), radius)
+    V = A - C
+    U = B - A
     e = (B - A).transpose()
     g = A - C
-    rsquare=norm(e + g) ** 2
-    alpha = np.sum((np.multiply(U,U)))
-    beta  =np.sum((np.multiply(U,V)))
-    gamma = np.sum((np.multiply(V,V))) - rsquare
-    t=[]
-    t.append((-beta)+math.sqrt(np.multiply(beta,beta)-np.multiply(alpha,gamma)))
-    t.append((-beta)-math.sqrt(np.multiply(beta,beta)-np.multiply(alpha,gamma)))
-    return np.multiply(np.outer(a0 + (b0-a0),limitt(t)),radian)
+    rsquare = norm(e + g) ** 2
+    alpha = np.sum((np.multiply(U, U)))
+    beta = np.sum((np.multiply(U, V)))
+    gamma = np.sum((np.multiply(V, V))) - rsquare
+    t = []
+    t.append((-beta) + math.sqrt(np.multiply(beta, beta) - np.multiply(alpha, gamma)))
+    t.append((-beta) - math.sqrt(np.multiply(beta, beta) - np.multiply(alpha, gamma)))
+    return np.multiply(np.outer(a0 + (b0 - a0), limitt(t)), radian)
+
+
 # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
@@ -95,16 +102,17 @@ with mp_hands.Hands(
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS, )
+                # mp_drawing.draw_landmarks(
+                #     image,
+                #     hand_landmarks,
+                #     mp_hands.HAND_CONNECTIONS, )
                 wrist = results.multi_hand_landmarks[0].landmark[0]
                 m1 = results.multi_hand_landmarks[0].landmark[5]
                 m2 = results.multi_hand_landmarks[0].landmark[17]
                 mid = midpoint(m1, m2)
 
-                points = [(mid[0], mid[1]), (wrist.x, wrist.y)]
+                points = [(mid[0] * image.shape[1], mid[1] * image.shape[0]),
+                          (wrist.x * image.shape[1], wrist.y * image.shape[0])]
                 x_coords, y_coords = zip(*points)
                 A = vstack([x_coords, ones(len(x_coords))]).T
                 m, c = lstsq(A, y_coords)[0]
@@ -112,34 +120,46 @@ with mp_hands.Hands(
 
 
                 def getLen():
-                    return (math.dist([wrist.x, wrist.y], [mid[0], mid[1]]) * image.shape[1]) / 2
+                    xx=(math.dist([wrist.x, wrist.y], [mid[0], mid[1]]) * image.shape[1]) / 2
+                    xx=70 if xx >70 else xx
+                    return xx
 
 
-                if wrist.x > mid[0]:
+                if wrist.x < mid[0]:
                     newX = wrist.x + (getLen() / image.shape[1])
                 else:
                     newX = wrist.x - (getLen() / image.shape[1])
                 newY = m * newX + c
 
-                c, a, b = (wrist.x, wrist.y), (wrist.x, wrist.y), (int(newX), int(newY))
-                r = getLen()
+                x, y = symbols('x y')
 
-                #print(c,a,b,r)
+                eq1 = Eq(x * m - y, -1 * c)
+                eq2 = Eq((x - (wrist.x * image.shape[1])) ** 2 + (y - (wrist.y * image.shape[0])) ** 2, getLen() ** 2)
+                ans = solve((eq1, eq2), (x, y))
 
-
-                # TODO Get intersection
-                #Dpoint(c,a,b,r)
-                print(Dpoint((48.137024, 11.575249), (48.139115, 11.578081), (48.146303, 11.593102), 1000.0))
-
-
-
-                image = cv2.circle(
-                    image,
-                    center=(int(newX * image.shape[1]), int(newY * image.shape[0])),
-                    radius=7,
-                    color=(0, 255, 0),
-                    thickness=-1,
-                )
+                # c, a, b = (wrist.x, wrist.y), (wrist.x, wrist.y), (newX, newY)
+                # r = getLen()
+                #
+                #
+                # # TODO Get intersection
+                # temp = Dpoint(c, a, b, r)
+                # newX = temp[0][0]
+                # newY = temp[1][0]
+                # print(Dpoint((48.137024, 11.575249), (48.139115, 11.578081), (48.146303, 11.593102), 1000.0))
+                try:
+                    image = cv2.circle(
+                        image,
+                        center=(int(ans[0][0]), int(ans[0][1])),
+                        radius=7,
+                        color=(0, 255, 0),
+                        thickness=-1,
+                    )
+                except:
+                    print("i")
+                # cv2.line(image,(int(mid[0] * image.shape[1]), int(mid[1] * image.shape[0])) , (int(newX),int(newY)),
+                #
+                #           (0, 255, 0), thickness=2)
+                # cv2.circle(image,(int(wrist.x* image.shape[1]),int(wrist.y* image.shape[0])), int(getLen()), (0,0,255), 0)
         cv2.imshow('MediaPipe Hands', image)
 
         if cv2.waitKey(5) & 0xFF == 27:
